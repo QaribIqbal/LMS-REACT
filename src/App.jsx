e081ee0b1ac4f074bd187f14e6f4d4017c110ffb
Qarib Iqbal <qaribiqbal46@pmxhq.com>
2025-06-29 19:31:14 +0500

    Fixed the navigation glitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
+// App.jsx
 import "bootstrap/dist/css/bootstrap.min.css";
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import { PrimeReactProvider } from 'primereact/api';
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import DashboardLayoutBasic from "./sidemenu.jsx";
 import { TaskProvider } from "./context.jsx";
-function App() {
 
+function App() {
   return (
-    <>
-    <PrimeReactProvider>
-      <TaskProvider>
-        <DashboardLayoutBasic />
-      </TaskProvider>
+    <BrowserRouter>
+      <PrimeReactProvider>
+        <TaskProvider>
+          <DashboardLayoutBasic />
+        </TaskProvider>
       </PrimeReactProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
